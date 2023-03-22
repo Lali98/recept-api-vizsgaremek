43722ded86a997a00d0e572a1e590688750f06c9
Lali98 <kisslajosbertalan77@gmail.com>
2023-03-22 10:40:52 +0100

    2023.03.22.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 const recipesApi = require("./router/recipe-routers");
 const usersApi = require("./router/user-routers");
 const categoriesApi = require("./router/category-routers");
+require('dotenv').config();
 
 const app = express();
 app.use(logger("dev"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/recipe-api", {useNewUrlParser: true})
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@clusterdatabase.l9w4xco.mongodb.net/receptek?retryWrites=true&w=majority`, {useNewUrlParser: true})
     .then(() => {
         console.log("MongoDB Connection successful");
         app.listen(9000, () => {
